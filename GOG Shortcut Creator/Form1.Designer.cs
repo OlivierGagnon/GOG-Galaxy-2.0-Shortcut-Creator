2ab2968f1d7fe0426b1d2b8595cbc65ae78bb9af
Olivier <powercat1@gmail.com>
2020-01-04 17:39:51 -0800

    initial
    initial

---
+++ GOG Shortcut Creator/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace GOG_Shortcut_Creator
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.textBoxGalaxyClient = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.browseGalaxyClient = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(12, 402);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(156, 23);
+            this.buttonCreate.TabIndex = 1;
+            this.buttonCreate.Text = "Create Shortcut to Desktop";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Location = new System.Drawing.Point(12, 373);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(156, 23);
+            this.buttonBrowse.TabIndex = 2;
+            this.buttonBrowse.Text = "Browse for GalaxyClient.exe";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // textBoxGalaxyClient
+            // 
+            this.textBoxGalaxyClient.Location = new System.Drawing.Point(174, 375);
+            this.textBoxGalaxyClient.Name = "textBoxGalaxyClient";
+            this.textBoxGalaxyClient.Size = new System.Drawing.Size(272, 20);
+            this.textBoxGalaxyClient.TabIndex = 4;
+            this.textBoxGalaxyClient.Text = "C:\\Program Files (x86)\\GOG Galaxy\\GalaxyClient.exe";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(434, 355);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // browseGalaxyClient
+            // 
+            this.browseGalaxyClient.FileName = "GalaxyClient.exe";
+            this.browseGalaxyClient.Filter = "GalaxyClient.exe|GalaxyClient.exe";
+            this.browseGalaxyClient.InitialDirectory = "C:\\Program Files (x86)\\GOG Galaxy\\";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(174, 407);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(270, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "(You can also double the above list to make a shortcut!)";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 432);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBoxGalaxyClient);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.buttonCreate);
+            this.Name = "Main";
+            this.Text = "GOG 2.0 Shortcut Creator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.TextBox textBoxGalaxyClient;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.OpenFileDialog browseGalaxyClient;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ GOG Shortcut Creator/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using IWshRuntimeLibrary;
+using System.IO;
+
+namespace GOG_Shortcut_Creator
+{
+    public partial class Main : Form
+    {        public Main()
+        {
+            InitializeComponent();
+            listBox1.Select();
+            List<Game> games = new List<Game>();
+            string cs = @"Data Source=C:\ProgramData\GOG.com\Galaxy\storage\galaxy-2.0.db;Version=3;";
+            SQLiteConnection con = new SQLiteConnection(cs);
+            con.Open();
+            string stm = @"SELECT trim(GamePieces.releaseKey,'gog_'), trim(trim(GamePieces.value,'{""title"":""'),'""}') FROM GamePieces
+                            JOIN GamePieceTypes ON GamePieces.gamePieceTypeId = GamePieceTypes.id
+                            WHERE releaseKey IN
+                            (SELECT Platforms.name || '_' || InstalledExternalProducts.productId FROM InstalledExternalProducts
+                            JOIN Platforms ON InstalledExternalProducts.platformId = Platforms.id)
+                            AND GamePieceTypes.type = 'originalTitle'";
+            SQLiteCommand cmd = new SQLiteCommand(stm, con);
+            SQLiteDataReader rdr = cmd.ExecuteReader();            
+            while (rdr.Read())
+            {
+                Game game = new Game();
+                game.name = rdr.GetString(1);
+                game.gameId = rdr.GetString(0);                
+                games.Add(game);
+                listBox1.Items.Add(game);
+            }
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            if (browseGalaxyClient.ShowDialog() == DialogResult.OK)
+            {
+                string file = browseGalaxyClient.FileName;
+                textBoxGalaxyClient.Text = file;
+            }
+        }
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            Game game = (Game)listBox1.SelectedItem;
+            CreateShortcut(game);
+        }
+        public static void CreateShortcut(Game game)
+        {
+            string invalid = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+            foreach (char c in invalid)
+            {
+                game.name = game.name.Replace(c.ToString(), "");
+            }
+            string shortcutLocation = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), game.name + ".lnk");
+            WshShell shell = new WshShell();
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutLocation);
+            shortcut.Description = $"Launch {game.name} through GOG Galaxy 2.0";
+            string path = "C:\\Program Files (x86)\\GOG Galaxy\\GalaxyClient.exe";
+            string args = "/command=runGame /gameId=" + game.gameId;
+            shortcut.TargetPath = path;
+            shortcut.Arguments = args;
+            shortcut.Save();
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            Game game = (Game)listBox1.SelectedItem;
+            CreateShortcut(game);
+        }
+    }
+    public class Game
+    {
+        public string name;
+        public string gameId;
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+}
