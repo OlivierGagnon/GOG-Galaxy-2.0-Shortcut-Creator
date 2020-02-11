5321e432b95fd9ef974217fb676d7350e5d6dd60
moweME <finn@bytesolutions.eu>
2020-02-11 23:23:22 +0100

    made form DPI aware upgraded .NET framework

--- GOG Shortcut Creator/Form1.Designer.cs
+++ GOG Shortcut Creator/Form1.Designer.cs
@@ -38,9 +38,10 @@ namespace GOG_Shortcut_Creator
             // 
             // buttonCreate
             // 
-            this.buttonCreate.Location = new System.Drawing.Point(12, 402);
+            this.buttonCreate.Location = new System.Drawing.Point(18, 618);
+            this.buttonCreate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonCreate.Name = "buttonCreate";
-            this.buttonCreate.Size = new System.Drawing.Size(156, 23);
+            this.buttonCreate.Size = new System.Drawing.Size(234, 35);
             this.buttonCreate.TabIndex = 1;
             this.buttonCreate.Text = "Create Shortcut to Desktop";
             this.buttonCreate.UseVisualStyleBackColor = true;
@@ -48,9 +49,10 @@ namespace GOG_Shortcut_Creator
             // 
             // buttonBrowse
             // 
-            this.buttonBrowse.Location = new System.Drawing.Point(12, 373);
+            this.buttonBrowse.Location = new System.Drawing.Point(18, 574);
+            this.buttonBrowse.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonBrowse.Name = "buttonBrowse";
-            this.buttonBrowse.Size = new System.Drawing.Size(156, 23);
+            this.buttonBrowse.Size = new System.Drawing.Size(234, 35);
             this.buttonBrowse.TabIndex = 2;
             this.buttonBrowse.Text = "Browse for GalaxyClient.exe";
             this.buttonBrowse.UseVisualStyleBackColor = true;
@@ -58,18 +60,21 @@ namespace GOG_Shortcut_Creator
             // 
             // textBoxGalaxyClient
             // 
-            this.textBoxGalaxyClient.Location = new System.Drawing.Point(174, 375);
+            this.textBoxGalaxyClient.Location = new System.Drawing.Point(261, 577);
+            this.textBoxGalaxyClient.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxGalaxyClient.Name = "textBoxGalaxyClient";
-            this.textBoxGalaxyClient.Size = new System.Drawing.Size(272, 20);
+            this.textBoxGalaxyClient.Size = new System.Drawing.Size(406, 26);
             this.textBoxGalaxyClient.TabIndex = 4;
             this.textBoxGalaxyClient.Text = "C:\\Program Files (x86)\\GOG Galaxy\\GalaxyClient.exe";
             // 
             // listBoxGames
             // 
             this.listBoxGames.FormattingEnabled = true;
-            this.listBoxGames.Location = new System.Drawing.Point(12, 12);
+            this.listBoxGames.ItemHeight = 20;
+            this.listBoxGames.Location = new System.Drawing.Point(18, 18);
+            this.listBoxGames.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.listBoxGames.Name = "listBoxGames";
-            this.listBoxGames.Size = new System.Drawing.Size(434, 355);
+            this.listBoxGames.Size = new System.Drawing.Size(649, 544);
             this.listBoxGames.TabIndex = 5;
             this.listBoxGames.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
             // 
@@ -82,23 +87,25 @@ namespace GOG_Shortcut_Creator
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(174, 407);
+            this.label1.Location = new System.Drawing.Point(261, 626);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(270, 13);
+            this.label1.Size = new System.Drawing.Size(400, 20);
             this.label1.TabIndex = 6;
             this.label1.Text = "(You can also double the above list to make a shortcut!)";
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(460, 432);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(690, 665);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listBoxGames);
             this.Controls.Add(this.textBoxGalaxyClient);
             this.Controls.Add(this.buttonBrowse);
             this.Controls.Add(this.buttonCreate);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.Name = "Main";
             this.Text = "GOG 2.0 Shortcut Creator";
